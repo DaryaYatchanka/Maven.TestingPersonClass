9a2dd4bd60ede574de662ce47d0361a9544708a6
Darya <Darya.yatchanka@gmail.com>
2019-06-01 14:54:16 -0400

    TestingPersonClassDone

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -1,35 +1,74 @@
 package com.zipcodewilmington.person;
 
+import java.util.Date;
+
 /**
  * Created by leon on 2/12/18.
  */
 public class Person {
     private String name;
     private int age;
+    private String address;
+    private Date dateOfBirth;
+    private int phoneNumber;
 
     public Person() {
     }
 
     public Person(int age) {
+        this.age=age;
+
     }
 
     public Person(String name) {
+         this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+
     }
 
     public void setName(String name) {
+        this.name = name;
+
+       // System.out.println();
     }
 
     public void setAge(int age) {
+       this.age=age;
     }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth){
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void getPhoneNumber(int phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
     public String getName() {
-        return null;
+
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+
+    public Date getDateOfBirth(){
+        return this.dateOfBirth;
+    }
+
+    public Integer getPhoneNumber(){
+        return this.phoneNumber;
     }
 }
